add8ffd850a14d6e1b72d82413556626dbbbae67
Sebastien Dupertuis <34378703+Sebdee@users.noreply.github.com>
2021-02-02 20:17:54 +0100

    Automated unit testing fully working
    4 test cases are fully operational and do provide nice representations of results.
    Based on this, an issue on the computed change has been found in the App.
    Last improvement can be to correct this issue.

--- MATLAB/Component/UnitTesting/TestApp.m
+++ MATLAB/Component/UnitTesting/TestApp.m
@@ -12,7 +12,9 @@ classdef TestApp < matlab.uitest.TestCase
       % Kill the running timer of the App
       evalin('base','stop(timerfind); delete(timerfind);');
       % Initialization
-      [nbSteps,nbParams] = size(responses);
+      xDataRef = (1:1:numel(stimuli))';
+      yDataRef = str2double(responses(2:end,:));
+      [nbSteps,nbParams] = size(testCase.App.AutoTestingTrace);
       xData = (1:1:nbSteps);
       yData = nan(nbSteps,nbParams);
       % Output sequence to expect for the correct behaviour
@@ -21,20 +23,28 @@ classdef TestApp < matlab.uitest.TestCase
           yData(m,n) = str2double(testCase.App.AutoTestingTrace{m+1,n});
         end
       end
+      yDataRef(nbSteps,:) = [0;0];
+      yData(nbSteps,:) = [0;0];
+      % Replace potential NaNs in the results by zeros
+      yDataRef(isnan(yDataRef)) = 0;
       yData(isnan(yData)) = 0;
       % Display results
       figure(testID);
+      stairs(xDataRef,yDataRef(:,1),'LineStyle','-','LineWidth',1.5); hold('on');
+      stairs(xDataRef,yDataRef(:,2),'LineStyle','-','LineWidth',1.5);
       stairs(xData,yData(:,1),'LineStyle','--','Marker','o','LineWidth',1.5);
-      hold('on');
       stairs(xData,yData(:,2),'LineStyle',':','Marker','d','LineWidth',1.5);
+      hold('off');
       xticks((1:1:nbSteps));
       xticklabels(categorical(stimuli));
+      xtickangle(45);
       xlabel('External events');
       ylabel('Amount of money');
       yticks((0:0.1:2));
       ytickformat('%.2f');
       title(['Results of test case #' num2str(testID)]);
-      legend('Still to pay','Given back');
+      legend('Expected: Still to pay','Expected: Given back',...
+             'Measured: Still to pay','Measured: Given back');
       % Verification of the produced output sequence against the reference
       testCase.verifyEqual(testCase.App.AutoTestingTrace,responses)
     end
@@ -68,8 +78,8 @@ classdef TestApp < matlab.uitest.TestCase
       testCase.press(testCase.App.ImageCoinSlot_1);
       pause(testCase.WaitTime);
       % Sequences to expect for the correct behaviour
-      stimuliTrace = {'Start' 'Green Button' '200 Cents' 'Wait' 'Grab Ticket'...
-                      'Grab Change' 'End'};
+      stimuliTrace = {'Start' 'Green Button' '200 Cents'...
+                      'Get Ticket' 'Wait' 'Get Change' 'End'};
       responsesTrace = {'DISP','COIN';'---',' ';'1.20',' ';...
                         ' ','0.50';' ','0.20';' ','0.10';'---',' '};
       testCase.outputsVerification(stimuliTrace,responsesTrace,1);
@@ -91,8 +101,8 @@ classdef TestApp < matlab.uitest.TestCase
       testCase.press(testCase.App.ImageCoinSlot_1);
       pause(testCase.WaitTime);
       % Sequences to expect for the correct behaviour
-      stimuliTrace = {'Start' 'Green Button' '100 Cents' 'Red Button'...
-                      'Grab Change' 'End'};
+      stimuliTrace = {'Start' 'Green Button' '100 Cents'...
+                      'Red Button' 'Get Change' 'End'};
       responsesTrace = {'DISP','COIN';'---',' ';'1.20',' ';...
                         '0.20',' ';' ','1.00';'---',' '};
       testCase.outputsVerification(stimuliTrace,responsesTrace,2);
@@ -121,11 +131,67 @@ classdef TestApp < matlab.uitest.TestCase
       pause(testCase.WaitTime);
       % Sequences to expect for the correct behaviour
       stimuliTrace = {'Start' 'Green Button' '100 Cents' '10 Cents' '200 Cents'...
-                      'Wait' 'Grab Ticket' 'Wait' 'Grab Change' 'End'};
+                      'Get Ticket' 'Wait' 'Wait' 'Get Change' 'End'};
       responsesTrace = {'DISP','COIN';'---',' ';'1.20',' ';'0.20',' ';...
                         '0.10',' ';' ','1.00';' ','0.50';' ','0.20';...
                         ' ','0.20';'---',' '};
       testCase.outputsVerification(stimuliTrace,responsesTrace,3);
+    end
+
+    function testCase_4_FullyEmptyAndReloadCashBox(testCase)
+      fprintf(' Processing the test case #4...\n');
+      pause(testCase.WaitTime);
+      for k=1:1:3
+        % Press the green button to start the purchase process
+        testCase.press(testCase.App.ButtonGreen);
+        pause(testCase.WaitTime);      
+        % Insert a coin of 100 cents in the App
+        testCase.press(testCase.App.ImageCoin_100);
+        pause(testCase.WaitTime);      
+        % Insert a coin of 10 cents in the App
+        testCase.press(testCase.App.ImageCoin_010);
+        pause(testCase.WaitTime);      
+        % Insert a coin of 200 cents in the App
+        testCase.press(testCase.App.ImageCoin_200);
+        pause(testCase.WaitTime);      
+        % Take back the ticket from the App
+        testCase.press(testCase.App.ImageTicket);
+        pause(testCase.WaitTime);
+        % Take back the change from the App
+        testCase.press(testCase.App.ImageCoinSlot_1);
+        pause(testCase.WaitTime);
+      end
+      % Press the key button to fully reload the cash box
+      testCase.press(testCase.App.ImageCashBoxKey);
+      pause(testCase.WaitTime);
+      % Press the green button to start the purchase process
+      testCase.press(testCase.App.ButtonGreen);
+      pause(testCase.WaitTime);      
+      % Insert a coin of 200 cents in the App
+      testCase.press(testCase.App.ImageCoin_200);
+      pause(testCase.WaitTime);      
+      % Take back the ticket from the App
+      testCase.press(testCase.App.ImageTicket);
+      pause(testCase.WaitTime);
+      % Take back the change from the App
+      testCase.press(testCase.App.ImageCoinSlot_1);
+      pause(testCase.WaitTime);
+      % Sequences to expect for the correct behaviour
+      stimuliTrace = {'Start' 'Green Button' '100 Cents' '10 Cents' '200 Cents'...
+                      'Get Ticket' 'Wait' 'Get Change' 'Wait' 'Green Button'...
+                      '100 Cents' '10 Cents' '200 Cents' 'Get Ticket' 'Wait'...
+                      'Get Change' 'Wait' 'Green Button' '100 Cents' '10 Cents'...
+                      '200 Cents' 'Get Ticket' 'Wait' 'Get Change' 'Reload Box'...
+                      'Green Button' '200 Cents' 'Get Ticket' 'Wait' 'Get Change' 'End'};
+      responsesTrace = {'DISP','COIN';'---',' ';'1.20',' ';'0.20',' ';...
+                        '0.10',' ';' ','1.00';' ','0.50';' ','0.20';...
+                        ' ','0.20';'---',' ';'1.20',' ';'0.20',' ';...
+                        '0.10',' ';' ','1.00';' ','0.50';' ','0.20';...
+                        ' ','0.20';'---',' ';'1.20',' ';'0.20',' ';...
+                        '0.10',' ';' ','1.00';' ','0.50';' ','0.20';...
+                        '---',' ';' ',' ';'1.20',' ';' ','0.50';...
+                        ' ','0.20';' ','0.10';'---',' '};
+      testCase.outputsVerification(stimuliTrace,responsesTrace,4);
     end
   end
 end
